30b625c13b3c0710300e0e691bea36325bd2f044
ade ali indra <adealiindra@gmail.com>
2023-08-25 17:21:41 +0700

    update

--- list.dart
+++ list.dart
@@ -1,10 +1,19 @@
-import 'dart:html';
+List<String> removeDuplicates(List<String> inputList) {
+  List<String> uniqueList = [];
+  for (String item in inputList) {
+    if (!uniqueList.contains(item)) {
+      uniqueList.add(item);
+    }
+  }
+  return uniqueList;
+}
 
-Map Alterr = {
-  'key1' = 'prima';
-  'key2' = 'muchson'; 
-};
+void main() {
+  List<String> input1 = ["amuse", "tommy kaira", "spoon", "HKS", "litchfield", "amuse", "HKS"];
+  List<String> output1 = removeDuplicates(input1);
+  print(output1);
 
-void main () {
-  Print 
+  List<String> input2 = ["JS Racing", "amuse", "spoon", "spoon", "JS Racing", "amuse"];
+  List<String> output2 = removeDuplicates(input2);
+  print(output2);
 }
---
+++ main.dart
@@ -0,0 +1,5 @@
+import 'data1.dart';
+
+//2 Buatlah sebuah fungsi yang akan menampilkan deskripsi dari barang dengan id 12.
+// ali
+
